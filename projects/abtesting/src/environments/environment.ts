3e4689073457dc8e4a9aac7059a79692b04cade7
Jaydip Hirapara <codecracker2455@gmail.com>
2020-05-22 22:32:23 +0530

    Update endpoint for env

--- projects/abtesting/src/environments/environment.ts
+++ projects/abtesting/src/environments/environment.ts
@@ -5,8 +5,7 @@
 
 const packageJson = require('../../../../package.json');
 
-const endpointApi = 'http://localhost:3030/api';
-// const endpointApi = 'http://development-upgrade-experiment-app.eba-gp6psjut.us-east-1.elasticbeanstalk.com/api';
+const endpointApi = 'http://development-upgrade-experiment-app.eba-gp6psjut.us-east-1.elasticbeanstalk.com/api';
 
 export const environment = {
   appName: 'UpGrade',
